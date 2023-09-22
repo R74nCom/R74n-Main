7ae5412609dbbe584e5cc694bcecccf6d358887e
slweeb <91897291+slweeb@users.noreply.github.com>
2023-09-22 12:29:43 -0400

    many tweaks

---
+++ multiplane/multiplane.js
@@ -0,0 +1,18 @@
+multiplaneEntities = {
+  "0": "nothing",
+  "1": "thing",
+  "2": "information",
+  "3": "action",
+  "4": "data",
+  "5": "online resource",
+  "6": "project",
+
+  "10": "test",
+
+// Projects //
+  "100": "R74n",
+}
+multiplanePlanes = {
+  "R00000-R000FF": "Fundamentals",
+  "R00100-R001FF": "Projects",
+}
