a89303d1a8038e64250e5af01b94953c48e5b017
slweeb <91897291+slweeb@users.noreply.github.com>
2023-09-22 23:03:42 -0400

    multiplane

--- multiplane/multiplane.js
+++ multiplane/multiplane.js
@@ -1,19 +1,73 @@
 // v1
 multiplaneEntities = {
+// Fundamentals //
   "0": "nothing",
   "1": "thing",
-  "2": "information",
-  "3": "action",
+  "2": "action",
+  "3": "information",
   "4": "data",
   "5": "online resource",
   "6": "project",
 
-  "10": "test",
+  "20": "test",
+  "21": "error",
+  "22": "unknown",
+  "23": "true",
+  "24": "false",
+
+  "80": "text",
+  "81": "text character",
+  "82": "word",
+  "83": "phrase",
+  "84": "transformed text",
+  "85": "language",
+  "86": "program",
+  "87": "webpage",
+  "88": "website",
+  "89": "video game",
+  "8A": "image",
+  "8B": "icon",
+  "8C": "audio",
+  "8D": "video",
+  "8E": "user account",
 
 // Projects //
   "100": "R74n",
+
+// Particulars //
+  "300": "R74n",
+
+
+  "(∅)2": "actionless",
 }
 multiplanePlanes = {
-  "R00000-R000FF": "Fundamentals",
-  "R00100-R001FF": "Projects",
+  "R00000-R00FFF": "Foundation",
+    "R00000-R000FF": "Fundamentals",
+    "R00100-R002FF": "Projects",
+    "R00300-R003FF": "Particulars",
+    "R00400-R004FF": "",
+  "R01000-R02FFF": "Project Spaces",
+    "R01000-R010FF": "",
+    "R01100-R011FF": "",
+    "R01200-R012FF": "",
+    "R01300-R013FF": "",
+  "R03000-R03FFF": "",
+  "R04000-R04FFF": "",
+}
+multiplaneDimensions = {
+  "": "Default",
+  "-": "Negative",
+  "i": "Imaginary",
+  "∅": "Null", // should be kept empty
+  "∞": "Infinite",
+  "↓": "Minimal",
+  "↑": "Maximal",
+  "½¹": "Half, First",
+  "½²": "Half, Second",
+  "∪": "Doublet",
+}
+
+
+multiplaneUnions = {
+  "R00003+R00088": "reference website",
 }
