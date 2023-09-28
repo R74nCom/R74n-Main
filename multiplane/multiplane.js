60d4b354b998c40a255ebfff0e35c22298e5dffb
slweeb <91897291+slweeb@users.noreply.github.com>
2023-09-27 21:41:19 -0400

    multiplane

--- multiplane/multiplane.js
+++ multiplane/multiplane.js
@@ -47,22 +47,22 @@ multiplaneEntities = {
   "98": "emoji",
   "99": "list",
   
-  "F0": "number 0",
-  "F1": "number 1",
-  "F2": "number 2",
-  "F3": "number 3",
-  "F4": "number 4",
-  "F5": "number 5",
-  "F6": "number 6",
-  "F7": "number 7",
-  "F8": "number 8",
-  "F9": "number 9",
-  "FA": "number A / 10",
-  "FB": "number B / 11",
-  "FC": "number C / 12",
-  "FD": "number D / 13",
-  "FE": "number E / 14",
-  "FF": "number F / 15",
+  "F0": "digit 0",
+  "F1": "digit 1",
+  "F2": "digit 2",
+  "F3": "digit 3",
+  "F4": "digit 4",
+  "F5": "digit 5",
+  "F6": "digit 6",
+  "F7": "digit 7",
+  "F8": "digit 8",
+  "F9": "digit 9",
+  "FA": "digit A / 10",
+  "FB": "digit B / 11",
+  "FC": "digit C / 12",
+  "FD": "digit D / 13",
+  "FE": "digit E / 14",
+  "FF": "digit F / 15",
 
 // Projects //
   "100": "R74n",
@@ -134,6 +134,79 @@ multiplaneEntities = {
   "549": "search",
   "560": "conversion",
 
+// Days Since R74n Epoch //
+  "11000": "May 2, 2017",
+  "12FFF": "October 5, 2039",
+// UTC Offsets //
+"13000":"UTC+0:00","13001":"UTC+0:15","13002":"UTC+0:30","13003":"UTC+0:45",
+"13004":"UTC+1:00","13005":"UTC+1:15","13006":"UTC+1:30","13007":"UTC+1:45",
+"13008":"UTC+2:00","13009":"UTC+2:15","1300A":"UTC+2:30","1300B":"UTC+2:45",
+"1300C":"UTC+3:00","1300D":"UTC+3:15","1300E":"UTC+3:30","1300F":"UTC+3:45",
+"13010":"UTC+4:00","13011":"UTC+4:15","13012":"UTC+4:30","13013":"UTC+4:45",
+"13014":"UTC+5:00","13015":"UTC+5:15","13016":"UTC+5:30","13017":"UTC+5:45",
+"13018":"UTC+6:00","13019":"UTC+6:15","1301A":"UTC+6:30","1301B":"UTC+6:45",
+"1301C":"UTC+7:00","1301D":"UTC+7:15","1301E":"UTC+7:30","1301F":"UTC+7:45",
+"13020":"UTC+8:00","13021":"UTC+8:15","13022":"UTC+8:30","13023":"UTC+8:45",
+"13024":"UTC+9:00","13025":"UTC+9:15","13026":"UTC+9:30","13027":"UTC+9:45",
+"13028":"UTC+10:00","13029":"UTC+10:15","1302A":"UTC+10:30","1302B":"UTC+10:45",
+"1302C":"UTC+11:00","1302D":"UTC+11:15","1302E":"UTC+11:30","1302F":"UTC+11:45",
+"13030":"UTC+12:00","13031":"UTC+12:15","13032":"UTC+12:30","13033":"UTC+12:45",
+"13034":"UTC+13:00","13035":"UTC+13:15","13036":"UTC+13:30","13037":"UTC+13:45",
+"13038":"UTC+14:00","13039":"UTC+14:15","1303A":"UTC+14:30","1303B":"UTC+14:45",
+"(-)13001":"UTC-0:15","(-)13002":"UTC-0:30","(-)13003":"UTC-0:45",
+"(-)13004":"UTC-1:00","(-)13005":"UTC-1:15","(-)13006":"UTC-1:30","(-)13007":"UTC-1:45",
+"(-)13008":"UTC-2:00","(-)13009":"UTC-2:15","(-)1300A":"UTC-2:30","(-)1300B":"UTC-2:45",
+"(-)1300C":"UTC-3:00","(-)1300D":"UTC-3:15","(-)1300E":"UTC-3:30","(-)1300F":"UTC-3:45",
+"(-)13010":"UTC-4:00","(-)13011":"UTC-4:15","(-)13012":"UTC-4:30","(-)13013":"UTC-4:45",
+"(-)13014":"UTC-5:00","(-)13015":"UTC-5:15","(-)13016":"UTC-5:30","(-)13017":"UTC-5:45",
+"(-)13018":"UTC-6:00","(-)13019":"UTC-6:15","(-)1301A":"UTC-6:30","(-)1301B":"UTC-6:45",
+"(-)1301C":"UTC-7:00","(-)1301D":"UTC-7:15","(-)1301E":"UTC-7:30","(-)1301F":"UTC-7:45",
+"(-)13020":"UTC-8:00","(-)13021":"UTC-8:15","(-)13022":"UTC-8:30","(-)13023":"UTC-8:45",
+"(-)13024":"UTC-9:00","(-)13025":"UTC-9:15","(-)13026":"UTC-9:30","(-)13027":"UTC-9:45",
+"(-)13028":"UTC-10:00","(-)13029":"UTC-10:15","(-)1302A":"UTC-10:30","(-)1302B":"UTC-10:45",
+"(-)1302C":"UTC-11:00","(-)1302D":"UTC-11:15","(-)1302E":"UTC-11:30","(-)1302F":"UTC-11:45",
+"(-)13030":"UTC-12:00","(-)13031":"UTC-12:15","(-)13032":"UTC-12:30","(-)13033":"UTC-12:45",
+"(-)13034":"UTC-13:00","(-)13035":"UTC-13:15","(-)13036":"UTC-13:30","(-)13037":"UTC-13:45",
+"(-)13038":"UTC-14:00","(-)13039":"UTC-14:15","(-)1303A":"UTC-14:30","(-)1303B":"UTC-14:45",
+// Extended Timekeeping //
+"13100":"Hour 0","13101":"Hour 1","13102":"Hour 2","13103":"Hour 3","13104":"Hour 4","13105":"Hour 5","13106":"Hour 6","13107":"Hour 7","13108":"Hour 8","13109":"Hour 9","1310A":"Hour 10","1310B":"Hour 11","1310C":"Hour 12","1310D":"Hour 13","1310E":"Hour 14","1310F":"Hour 15","13110":"Hour 16","13111":"Hour 17","13112":"Hour 18","13113":"Hour 19","13114":"Hour 20","13115":"Hour 21","13116":"Hour 22","13117":"Hour 23",
+"13120":"Minute 0","13121":"Minute 1","13122":"Minute 2","13123":"Minute 3","13124":"Minute 4","13125":"Minute 5","13126":"Minute 6","13127":"Minute 7","13128":"Minute 8","13129":"Minute 9","1312A":"Minute 10","1312B":"Minute 11","1312C":"Minute 12","1312D":"Minute 13","1312E":"Minute 14","1312F":"Minute 15","13130":"Minute 16","13131":"Minute 17","13132":"Minute 18","13133":"Minute 19","13134":"Minute 20","13135":"Minute 21","13136":"Minute 22","13137":"Minute 23","13138":"Minute 24","13139":"Minute 25","1313A":"Minute 26","1313B":"Minute 27","1313C":"Minute 28","1313D":"Minute 29","1313E":"Minute 30","1313F":"Minute 31","13140":"Minute 32","13141":"Minute 33","13142":"Minute 34","13143":"Minute 35","13144":"Minute 36","13145":"Minute 37","13146":"Minute 38","13147":"Minute 39","13148":"Minute 40","13149":"Minute 41","1314A":"Minute 42","1314B":"Minute 43","1314C":"Minute 44","1314D":"Minute 45","1314E":"Minute 46","1314F":"Minute 47","13150":"Minute 48","13151":"Minute 49","13152":"Minute 50","13153":"Minute 51","13154":"Minute 52","13155":"Minute 53","13156":"Minute 54","13157":"Minute 55","13158":"Minute 56","13159":"Minute 57","1315A":"Minute 58","1315B":"Minute 59",
+"13160":"Second 0","13161":"Second 1","13162":"Second 2","13163":"Second 3","13164":"Second 4","13165":"Second 5","13166":"Second 6","13167":"Second 7","13168":"Second 8","13169":"Second 9","1316A":"Second 10","1316B":"Second 11","1316C":"Second 12","1316D":"Second 13","1316E":"Second 14","1316F":"Second 15","13170":"Second 16","13171":"Second 17","13172":"Second 18","13173":"Second 19","13174":"Second 20","13175":"Second 21","13176":"Second 22","13177":"Second 23","13178":"Second 24","13179":"Second 25","1317A":"Second 26","1317B":"Second 27","1317C":"Second 28","1317D":"Second 29","1317E":"Second 30","1317F":"Second 31","13180":"Second 32","13181":"Second 33","13182":"Second 34","13183":"Second 35","13184":"Second 36","13185":"Second 37","13186":"Second 38","13187":"Second 39","13188":"Second 40","13189":"Second 41","1318A":"Second 42","1318B":"Second 43","1318C":"Second 44","1318D":"Second 45","1318E":"Second 46","1318F":"Second 47","13190":"Second 48","13191":"Second 49","13192":"Second 50","13193":"Second 51","13194":"Second 52","13195":"Second 53","13196":"Second 54","13197":"Second 55","13198":"Second 56","13199":"Second 57","1319A":"Second 58","1319B":"Second 59",
+"131A0":"January","131A1":"February","131A2":"March","131A3":"April","131A4":"May","131A5":"June","131A6":"July","131A7":"August","131A8":"September","131A9":"October","131AA":"November","131AB":"December",
+"131B0":"Sunday","131B1":"Monday","131B2":"Tuesday","131B3":"Wednesday","131B4":"Thursday","131B5":"Friday","131B6":"Saturday",
+
+// Colors //
+"13200":"red",
+"13201":"red-orange",
+"13202":"orange",
+"13203":"yellow-orange",
+"13204":"yellow",
+"13205":"chartreuse",
+"13206":"lime",
+"13207":"cyan",
+"13208":"blue",
+"13209":"neon indigo",
+"1320A":"magenta",
+"1320B":"hot pink",
+
+"13210":"dark red",
+"13212":"brown",
+"13213":"golden",
+"13214":"dark yellow",
+"13216":"green",
+"13217":"teal",
+"13219":"indigo",
+"1321A":"purple",
+
+"13220":"pink",
+"13224":"beige",
+"13227":"light cyan",
+
+"1320D":"black",
+"1320E":"gray",
+"1321E":"dark gray",
+"1322E":"light gray",
+"1320F":"white",
 
   "(-)0": "something",
   "(∞)1": "everything",
@@ -162,18 +235,56 @@ multiplanePlanes = {
     "R00300-R003FF": "Particulars",
     "R00400-R004FF": "Resource Types",
     "R00500-R005FF": "Actions",
+    "R00F00-R00FFF": "Meta",
   "R01000-R0FFFF": "Project Spaces",
     "R01000-R010FF": "",
     "R01100-R011FF": "",
     "R01200-R012FF": "",
     "R01300-R013FF": "",
   "R10000-R1FFFF": "Descriptors",
+    "R10000-R10FFF": "Countries & Regions",
+    "R11000-R12FFF": "Days Since R74n Epoch",
+    "R13000-R130FF": "UTC Offsets",
+    "R13100-R131FF": "Extended Timekeeping",
+    "R13200-R132FF": "Colors",
+    "R1B000-R1FFFF": "Numeral Compatibility", //Numbers 0-20478
   "R20000-R20FFF": "",
   "R21000-R21FFF": "",
   "R22000-R22FFF": "",
   "RD0000-RDFFFF": "Wikibase Item Compatibility",
   "RE0000-REFFFF": "Ontomata Class Compatibility",
   "RF0000-RFFFFF": "Private Use Plane",
+}
+
+multiplaneProcedural = {
+  "R11000-R12FFF": function(decimal,dimension) {
+    var date = new Date(2017, 4, 2);
+    if (dimension === "") { date.setDate(date.getDate() + (decimal-69632)); }
+    else if (dimension === "-" && decimal > 69632) { date.setDate(date.getDate() - (decimal-69632)); }
+    else { return "" }
+    return date.toLocaleString('default', { month: 'long' }) + " " + date.getDate() + ", " + date.getFullYear();
+  },
+  "R1B000-R1FFFF": function(decimal,dimension) {
+    if (dimension === "") {
+      return "Number " + (decimal-110592);
+    }
+    if (dimension === "-" && decimal > 110592) {
+      return "Number -" + (decimal-110592);
+    }
+    return "";
+  },
+  "RD0000-RDFFFF": function(decimal,dimension) {
+    if (dimension === "" && decimal > 851968) {
+      return "Q" + (decimal-851968);
+    }
+    return "";
+  },
+  "RE0000-REFFFF": function(decimal,dimension) {
+    if (dimension === "" && decimal > 917504) {
+      return "Class #" + (decimal-917504);
+    }
+    return "";
+  },
 }
 
 
