dff8fd4f13b889f42a9f2fae1a256bf5960d3e74
slweeb <91897291+slweeb@users.noreply.github.com>
2024-03-04 21:32:07 -0500

    .

---
+++ cook/foodData.js
@@ -0,0 +1,55 @@
+ingredients = {
+cheese: {
+    h:50,s:100,l:50,
+    type:"dairy",
+    shape:"circle_porous",
+    adj:"cheesy"
+},
+red_cheese: {
+    h:0,s:100,l:50,
+    type:"dairy",
+    shape:"circle_porous",
+    adj:"cheesy"
+},
+orange_cheese: {
+    h:25,s:100,l:50,
+    type:"dairy",
+    shape:"circle_porous",
+    adj:"cheesy"
+},
+green_cheese: {
+    h:100,s:100,l:50,
+    type:"dairy",
+    shape:"circle_porous",
+    adj:"cheesy"
+},
+blue_cheese: {
+    h:200,s:100,l:50,
+    type:"dairy",
+    shape:"circle_porous",
+    adj:"cheesy"
+},
+purple_cheese: {
+    h:300,s:100,l:50,
+    type:"dairy",
+    shape:"circle_porous",
+    adj:"cheesy"
+},
+black_cheese: {
+    h:0,s:0,l:20,
+    type:"dairy",
+    shape:"circle_porous",
+    adj:"cheesy"
+},
+white_cheese: {
+    h:0,s:0,l:75,
+    type:"dairy",
+    shape:"circle_porous",
+    adj:"cheesy"
+},
+not_cheese: {
+    h:0,s:50,l:90,
+    type:"dairy",
+    shape:"sus",
+},
+}
