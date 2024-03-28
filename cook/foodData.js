17dadf5393f4f0853443dd6326b833de7e0c3f25
slweeb <91897291+slweeb@users.noreply.github.com>
2024-03-27 20:56:44 -0400

    shapes

--- cook/foodData.js
+++ cook/foodData.js
@@ -2,7 +2,7 @@
 [Ingredient Properties]
 name: override default name
 type: inherits properties from specified ingredient
-group: other(default), generic(hidden), dairy, mineral, carb, fruit
+group: other(default), generic(hidden), dairy, mineral, carb, fruit, vegetable, meat
 shape: R74n Shapes file name, without .png
 behavior: 0=default, 1=liquid, 2=powder
 adj: adjective to describe ingredient in dish
@@ -21,6 +21,8 @@ powder: {
     shape:"powder",
     behavior:2
 },
+
+
 water: {
     type:"liquid",
     color:"#bfd8df",
@@ -33,10 +35,28 @@ oil: {
     type:"liquid",
     color:"#e7df97",
 },
-egg: {
-    shape:"ovoid",
-    color:"#F0EAD6",
+milk: {
+    type:"liquid",
+    group: "dairy",
+    color:"#f3f3ec",
 },
+yogurt: {
+    group: "dairy",
+    shape: "clumps",
+    color:"#f3f3ec",
+},
+fat: {
+    group: "meat",
+    shape: "scoop",
+    color:"#f3f3ec",
+},
+butter: {
+    group: "dairy",
+    shape: "scoop",
+    color:"#ffff80",
+},
+
+
 salt: {
     type:"powder",
     group:"mineral",
@@ -54,15 +74,72 @@ flour: {
     shape:"powder_rough",
     color:"#f4efe5",
 },
+
+
+egg: {
+    shape:"ovoid",
+    color:"#F0EAD6",
+},
 cheese: {
     color:"#fec118",
     group:"dairy",
     shape:"wedge",
     adj:"cheesy"
 },
-red_apple: {
+
+
+apple: {
     color:"#ff1f40",
     group:"fruit",
     shape:"fruit_bipod_stem"
+},
+leaf_vegetable: {
+    color:"#41d841",
+    group:"vegetable",
+    shape:"leaf_vegetable"
+},
+herb: {
+    color:"#35b135",
+    group:"vegetable",
+    shape:"leaf"
+},
+
+
+noodles: {
+    color:"#d2cdad",
+    group:"carb",
+    shape:"noodles"
+},
+bread: {
+    color:"#ddc69c",
+    group:"carb",
+    shape:"loaf"
+},
+
+
+meat: {
+    color:"#ff6e78",
+    group:"meat",
+    shape:"cutlet"
+},
+poultry: {
+    color:"#ffdddf",
+    type:"meat",
+    shape:"poultry"
+},
+fish: {
+    color:"#4edeff",
+    type:"meat",
+    shape:"fish"
+},
+crustacean: {
+    color:"#f13851",
+    type:"meat",
+    shape:"crustacean"
+},
+cephalopod: {
+    color:"#ffadd1",
+    type:"meat",
+    shape:"cephalopod"
 },
 }
