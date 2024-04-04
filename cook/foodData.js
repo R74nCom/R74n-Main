73f8ba6426a21433687ff0920cb1e4e5f87343d1
slweeb <91897291+slweeb@users.noreply.github.com>
2024-04-03 23:41:59 -0400

    cook

--- cook/foodData.js
+++ cook/foodData.js
@@ -4,10 +4,18 @@ name: override default name
 type: inherits properties from specified ingredient
 group: other(default), generic(hidden), dairy, mineral, carb, fruit, vegetable, meat, egg
 shape: R74n Shapes file name, without .png
+placedShape: Shape used instead when placed
 behavior: 0=default, 1=liquid, 2=powder
 adj: adjective to describe ingredient in dish
 hidden: true=hidden from ingredient list
-color, r, g, b, h, s, l, rgb, hsl
+keywords: extra text to check when searching
+short: short name for ingredient
+dissolve: true=hides in liquid
+delete: true=hides when dropped
+height: hitbox height multiplier (e.g. 0.5 will halve it)
+color: #hex or array of #hex
+a: opacity from 0-1
+h, s, l, r, g, b, rgb, hsl
 */
 
 shapeMeta = {
@@ -18,11 +26,20 @@ ingredients = {
 liquid: {
     group:"generic",
     shape:"liquid",
+    placedShape:"droplets_some",
     behavior:1
+},
+thick_liquid: {
+    type:"liquid",
+    group:"generic",
+    shape:"liquid",
+    placedShape:"droplets_some",
+    behavior:2
 },
 powder: {
     group:"generic",
     shape:"powder",
+    placedShape:"squares_some",
     behavior:2
 },
 
@@ -32,14 +49,26 @@ water: {
     color:"#bfd8df",
     adj:"soggy"
 },
-ketchup: {
+broth: {
     type:"liquid",
+    color:"#dbcda6",
+    dishName:"soup",
+    dishWeight:1000
+},
+ice_cube: {
+    color:"#b4efff",
+    a:0.75,
+    shape:"cube",
+    adj:"iced"
+},
+ketchup: {
+    type:"thick_liquid",
     color:"#ff2b2b",
     group:"vegetable",
     keywords: "catsup"
 },
 mayonnaise: {
-    type:"liquid",
+    type:"thick_liquid",
     color:"#f9f5e5",
     group:"egg",
     short:"mayo"
@@ -47,11 +76,13 @@ mayonnaise: {
 oil: {
     type:"liquid",
     color:"#e7df97",
+    a: 0.66
 },
 milk: {
     type:"liquid",
     group: "dairy",
     color:"#f3f3ec",
+    keywords:"dairy"
 },
 yogurt: {
     group: "dairy",
@@ -72,6 +103,7 @@ ice_cream: {
     group: "dairy",
     shape: "scoop",
     color:"#fffdf4",
+    keywords:"sundae"
 },
 
 
@@ -80,11 +112,15 @@ salt: {
     group:"mineral",
     shape:"powder_rough",
     color:"#e3e3e3",
+    dissolve:true,
+    adj:"salted"
 },
 sugar: {
     type:"powder",
     group:"carb",
     color:"#f2f2f2",
+    dissolve:true,
+    adj:"sweet"
 },
 flour: {
     type:"powder",
@@ -133,7 +169,8 @@ leaf_vegetable: {
 herb: {
     color:"#35b135",
     group:"vegetable",
-    shape:"leaf"
+    shape:"leaf",
+    adj:"herbal"
 },
 
 
@@ -163,23 +200,30 @@ bun: {
 },
 bottom_bun: {
     type:"bun",
-    shape:"rectangle_thin_round"
+    shape:"rectangle_thin_round",
+    dishName:"bun"
 },
 top_bun: {
     type:"bun",
     shape:"semicircle_top",
-    height:0.75
+    height:0.75,
+    dishName:"bun"
 },
 
 
 meat: {
     color:"#ff6e78",
     group:"meat",
-    shape:"cutlet"
+    shape:"cutlet",
+    hidden:true,
+    reactions: {
+        water: { "set2":"broth" }
+    },
 },
 beef: {
     color:"#ff4d58",
-    type:"meat"
+    type:"meat",
+    keywords:"meat steak"
 },
 beef_patty: {
     type:"beef",
@@ -188,23 +232,56 @@ beef_patty: {
 poultry: {
     color:"#ffdddf",
     type:"meat",
-    shape:"poultry"
+    shape:"poultry",
+    keywords:"bird"
 },
 fish: {
     color:"#4edeff",
     type:"meat",
-    shape:"fish"
+    shape:"fish",
+    keywords:"seafood pescetarian"
 },
 crustacean: {
     color:"#f13851",
     type:"meat",
-    shape:"crustacean"
+    shape:"crustacean",
+    hidden:true
+},
+crab: {
+    type:"crustacean",
+    keywords:"crustacean seafood"
 },
 cephalopod: {
     color:"#ffadd1",
     type:"meat",
-    shape:"cephalopod"
+    shape:"cephalopod",
+    hidden:true
 },
+squid: {
+    type:"cephalopod",
+    keywords:"cephalopod seafood"
+},
+
+
+dye: {
+    color:["#ff0000","#ffff00","#00ff00","#00ffff","#0000ff","#ff00ff"],
+    type:"powder",
+    hidden:true,
+    stain:true,
+    adj:"dyed",
+    shape:"powder_rough",
+    dishWeight:-1000,
+    delete:true
+},
+red_dye: {
+    color:"#ff0000",
+    type:"dye",
+    stain:true,
+    adj:"red"
+},
+
+
+
 }
 
 
@@ -214,5 +291,6 @@ dishRecipes = {
 "mac and cheese+bread": "breaded mac",
 "bun+beef+bun": "hamburger",
 "hamburger+cheese": "cheeseburger",
+"meat+water": "broth",
 
 }
