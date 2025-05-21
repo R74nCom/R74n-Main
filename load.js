9a440f57471c15e00895608cf0a9d358708459c7
slweeb <91897291+slweeb@users.noreply.github.com>
2025-05-21 14:40:15 -0400

    .

--- load.js
+++ load.js
@@ -88,7 +88,7 @@ function playSound(url) {
 /*R74n Observer
   Don't worry, I'm harmless!*/
 if (document.referrer && document.referrer.indexOf("r74n.") === -1) {
-	const refdomain = document.referrer.replace(/^https?:\/\//g, "").replace(/\/$/g, "");
+	const refdomain = document.referrer.replace(/^https?:\/\//g, "").replace(/^www\./g, "").replace(/\/$/g, "");
     document.cookie = "R74nRef="+refdomain+";max-age=86400;path=/;domain=r74n.com";
 }
 
