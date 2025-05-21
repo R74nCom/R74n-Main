12afc1bde5e08ea53c087e31b96d7e2a4a91a540
slweeb <91897291+slweeb@users.noreply.github.com>
2025-05-21 13:54:34 -0400

    .

--- load.js
+++ load.js
@@ -87,7 +87,7 @@ function playSound(url) {
   Don't worry, I'm harmless!*/
 if (document.referrer && document.referrer.indexOf("r74n.") === -1) {
 	const refdomain = document.referrer.replace(/^https?:\/\//g, "").replace(/\/$/g, "");
-    document.cookie = "R74nRef="+document.referrer+";max-age=86400;path=/;domain=r74n.com";
+    document.cookie = "R74nRef="+refdomain+";max-age=86400;path=/;domain=r74n.com";
 }
 
 window.addEventListener("load",function(){
--- observe.js
+++ observe.js
@@ -2,5 +2,5 @@
   Don't worry, I'm harmless!*/
 if (document.referrer && document.referrer.indexOf("r74n.") === -1) {
 	const refdomain = document.referrer.replace(/^https?:\/\//g, "").replace(/\/$/g, "");
-    document.cookie = "R74nRef="+document.referrer+";max-age=86400;path=/;domain=r74n.com";
+    document.cookie = "R74nRef="+refdomain+";max-age=86400;path=/;domain=r74n.com";
 }
