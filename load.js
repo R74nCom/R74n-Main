843c98b92b95786eea92c935b1158208e5148e84
slweeb <91897291+slweeb@users.noreply.github.com>
2026-02-11 21:59:48 -0500

    .

--- load.js
+++ load.js
@@ -505,6 +505,7 @@ R74n.more = function() {
 	<a href="${R74n.root}mini/handwriting" class="new" style="background-image: url(${R74n.root}mini/spotlight-handwriting.png);">Handwriting Personality</a>
 	<a href="${R74n.root}ants/" style="background-image: url(${R74n.root}ants/spotlight.png);">Every Ant on Earth</a>
 	<a href="${R74n.root}pixelflags/guess" style="background-image: url(${R74n.root}pixelflags/spotlight.png);">Guess the Pixel Flag</a>
+	<a href="${R74n.root}mix/" style="background-image: url(${R74n.root}mix/spotlight.png);">Mix-Up!</a>
 </div></div>
 `
 }
